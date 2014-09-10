09b345d5860147146ee6386aed3ed6e2255321a7
Brian McDonald <brian.mcdonald1980@googlemail.com>
2014-09-10 15:37:16 +0100

    Add functions for Init and Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,16 @@
 
 //global values go here!
 
+//Global functions
+void InitWindows(int width, int height, bool fullscreen)
+{
+
+}
+
+void CleanUp()
+{
+
+}
 
 //Main Method
 int main(int argc, char * arg[])
