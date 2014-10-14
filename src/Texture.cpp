e1a1fbe0281bb1951c61c6ba88e9bbce72be6e55
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2014-10-14 22:04:39 +0100

    Moved close Font

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -78,12 +78,12 @@ GLuint loadTextureFromFont(const std::string& fontFilename, int pointSize, const
 
 	textureID=convertSDLSurfaceToGLTexture(textSurface);
 
-	TTF_CloseFont(font);
-
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	TTF_CloseFont(font);
 
 	return textureID;
 }
